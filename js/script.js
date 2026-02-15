4e980ec7f99cb12f54248ad2a1a6837cd39e9f5d
Lwowsky <vovapotaychuk@gmail.com>
2026-02-15 10:13:54 +0900

    responsive

---
+++ js/script.js
@@ -0,0 +1,33 @@
+(() => {
+  const el = document.getElementById("yearRange");
+  if (!el) return;
+
+  const start = Number(el.dataset.start) || new Date().getFullYear();
+  const now = new Date().getFullYear();
+
+  el.textContent = (start === now) ? `${now}` : `${start}–${now}`;
+})();
+
+const photos = [
+  "img/hero/photo1.jpg",
+  "img/hero/photo2.jpg",
+  "img/hero/photo3.jpg",
+  "img/hero/photo4.jpg",
+  "img/hero/photo5.jpg",
+  "img/hero/photo6.jpg",
+  "img/hero/photo7.jpg",
+  "img/hero/photo8.jpg",
+  "img/hero/photo9.jpg",
+  "img/hero/photo10.jpg",
+  "img/hero/photo11.jpg",
+  "img/hero/photo12.jpg",
+  "img/hero/photo13.jpg",
+];
+
+let i = 0;
+const img = document.getElementById("heroPhoto");
+
+setInterval(() => {
+  i = (i + 1) % photos.length;
+  img.src = photos[i];
+}, 3500);
