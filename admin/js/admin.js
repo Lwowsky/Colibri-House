58f5ce530d13a76f42a6bc6b2a7c82565088e45b
Lwowsky <vovapotaychuk@gmail.com>
2026-03-28 19:59:51 +0900

    іва

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -304,11 +304,9 @@
 
                 <label class="menu-item-image-path">
                   Image path / URL
-                  <input data-field="img" type="text" value="${escapeHtml(item.img || "")}" placeholder="img/menu/item.jpg or /content/uploads/menu/item.jpg" />
+                  <input data-field="img" type="text" value="${escapeHtml(item.img || "")}" placeholder="img/menu/item.jpg or content/uploads/menu/item.jpg" />
                 </label>
-              </div>
 
-              <aside class="menu-item-media">
                 <div class="uploader">
                   <label>
                     Upload image to GitHub
@@ -316,6 +314,9 @@
                   </label>
                   <small class="muted">Файл завантажиться в <code>content/uploads/menu</code> і шлях підставиться автоматично.</small>
                 </div>
+              </div>
+
+              <aside class="menu-item-media">
                 <div class="image-preview" data-image-preview>${preview}</div>
               </aside>
             </div>
